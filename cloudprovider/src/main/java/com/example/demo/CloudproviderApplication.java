3e490669c847a296c9c0fa690bda2cbbb7651b06
zhangqi234110 <314890945@qq.com>
2019-10-07 17:46:50 +0800

    注册中心的搭建

---
+++ cloudprovider/src/main/java/com/example/demo/CloudproviderApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudproviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudproviderApplication.class, args);
+	}
+
+}
